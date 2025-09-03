725d929f7fef91cf807af4b0a1b8218fb6d58f6d
aDiThYa-808 <adithyas824@gmail.com>
2025-09-03 21:43:43 +0530

    feat(cli): update valid ports in start command(1024-65535).

--- src/cli/commands/start.ts
+++ src/cli/commands/start.ts
@@ -3,7 +3,7 @@ import { startServer } from "../../core/server.js";
 export function startHandler(port:string):void{
     const selectedPortNumber = parseInt(port, 10);
     if(!isValidPort(selectedPortNumber)){
-        console.log("Invalid port number.");
+        console.log(`Invalid port number: ${selectedPortNumber}. Please provide a valid port number between 1024 and 65535.`);
     }
     else{
         startServer(selectedPortNumber);
@@ -13,7 +13,7 @@ export function startHandler(port:string):void{
 
 //helper functions
 function isValidPort(port:number):boolean{
-    if(Number.isNaN(port) || !Number.isInteger(port) || port< 1 || port > 65535){
+    if(Number.isNaN(port) || !Number.isInteger(port) || port< 1024 || port > 65535){
         return false
     }
     return true
