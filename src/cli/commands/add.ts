d86e33b3039797e57ca98964e95d31606a967c78
aDiThYa-808 <adithyas824@gmail.com>
2025-09-06 16:35:26 +0530

    feat(cli): write new endpoints into testapi.json

--- src/cli/commands/add.ts
+++ src/cli/commands/add.ts
@@ -1,8 +1,25 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import type { addOptions } from "../../utils/commandOptions";
+import * as path from "path";
 
 export function addHandler(options:addOptions):void{
-    console.log(`method:${options.method}`);
-    console.log(`path:${options.path}`);
-    console.log(`response:${options.response}`);
-    console.log(`status:${options.status}`);
+    const newEndpoint = {
+        'method': options.method,
+        'path': options.path,
+        'response': JSON.parse(options.response),
+        'status': options.status
+    };
+
+    const configFilePath = path.join(process.cwd(),"testapi.json");
+    
+    if(!existsSync(configFilePath)){
+        console.log("testapi not initialized. Use init.");
+    }
+    else{
+        const configFileContents = readFileSync(configFilePath,"utf-8");
+        const data = JSON.parse(configFileContents);
+        data.endpoints.push(newEndpoint);
+        const newConfigFileContent = JSON.stringify(data,null,2);
+        writeFileSync(configFilePath,newConfigFileContent,"utf-8")
+    }
 }
