6fb09acdf2110c433c12a4178865e5dce4976016
aDiThYa-808 <adithyas824@gmail.com>
2025-09-07 15:11:30 +0530

    feat(cli): add validation helpers for addHandler

--- src/cli/commands/add.ts
+++ src/cli/commands/add.ts
@@ -2,6 +2,8 @@ import { existsSync, readFileSync, writeFileSync } from "fs";
 import type { addOptions } from "../../utils/commandOptions";
 import * as path from "path";
 
+const validMethods = ['GET','POST','PUT','DELETE'];
+
 export function addHandler(options:addOptions):void{
     const newEndpoint = {
         'method': options.method,
@@ -11,15 +13,32 @@ export function addHandler(options:addOptions):void{
     };
 
     const configFilePath = path.join(process.cwd(),"testapi.json");
-    
-    if(!existsSync(configFilePath)){
-        console.log("testapi not initialized. Use init.");
+
+    try{
+        if(doesConfigFileExist(configFilePath) && isValidMethod(options.method)){
+            const configFileContents = readFileSync(configFilePath,"utf-8");
+            const data = JSON.parse(configFileContents);
+            data.endpoints.push(newEndpoint);
+            const newConfigFileContent = JSON.stringify(data,null,2);
+            writeFileSync(configFilePath,newConfigFileContent,"utf-8")
+        }
     }
-    else{
-        const configFileContents = readFileSync(configFilePath,"utf-8");
-        const data = JSON.parse(configFileContents);
-        data.endpoints.push(newEndpoint);
-        const newConfigFileContent = JSON.stringify(data,null,2);
-        writeFileSync(configFilePath,newConfigFileContent,"utf-8")
+    catch(err){
+        console.log((err as Error).message);
     }
+}
+
+//helper functions
+function isValidMethod(method:string):Boolean{
+    if(!validMethods.includes(method)){
+        throw new Error("Invalid method. Try GET,POST,PUT or DELETE.")
+    }
+    return true;
+}
+
+function doesConfigFileExist(path:string):Boolean{
+    if(!existsSync(path)){
+        throw new Error("testapi.json not found. Use 'init' to create one.");
+    }
+    return true;
 }
